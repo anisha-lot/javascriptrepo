1b84089195e57086ef378e08a1efe0103bacd414
anisha-lot <anishalot@gmail.com>
2024-05-28 08:29:30 +0000

    variable & keywords

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 2822
+let accountEmail = "abcgoogle.com"
+var accountPassword = "anii!12"
+accountCity = "pune" 
+let accountState;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+ //var is dont use beacause it is not block scope
+ //you can declare a variable without keyword but it not use most of the time 
+
