77a7354efab8c4235f8bb4916350c5710c624114
anisha-lot <anishalot@gmail.com>
2024-05-28 14:36:32 +0000

    type conversion

---
+++ 01_basics/03_DatatTypeconversion.js
@@ -0,0 +1,39 @@
+//let score= undefined
+//console.log(typeof score)
+
+//let valueofNumber = Number(score)  //in that code of line declare a variable valueofnumber in that the value of score is always print as a number 
+// means type is that score is always a number
+//console.log(valueofNumber)
+//console.log(typeof valueofNumber)
+
+/*
+if we declare a number then its type is always a number
+but it write in a string then o/p is NaN (not a number)
+we can type code in that form the type can become a number if there is string 
+but in actual that a nit a number 
+*/
+
+// if 33 - type is number and actual is also a number
+// "33"  -  type can be a number but actual is NaN (not a number)
+// if value is null -  type can be set as number but in realitiy its value is 0 (it may be use for forcasting whether)
+// undefind -  type can be make number but its realitiy is NaN
+// boolean -  actual value of true is =1 - false =0  but type can make a number
+
+
+//let loggedIn = "" 
+
+//let booleanloggedIn = Boolean(loggedIn)
+//console.log(booleanloggedIn)
+
+// 1= true
+// "" = false 
+// 0- false
+// "hitesh" = true
+
+  let someNumber = 22
+ 
+ let stringNumber= String(someNumber)
+ console.log(stringNumber)
+ console.log(typeof stringNumber)
+
+ //o/p show as number but type is string
