550e427b9f0b5b21647acbb2d7afe70ce7b6c9af
anisha-lot <anishalot@gmail.com>
2024-05-28 08:28:53 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+ "use strict" //treat js as newer version 
+
+ //alert(3+3) //we use node js not a browser 
+ // we can write this on console bar of browser inspect but not use this type of code when you use node js
+
+ // write code as a redability 
+
+  let age = 24
+  let employeName= "anisha lot"
+  let accountLogin = "no"
+  //let userState = null  o/p is null
+  // let userName; o/p is undefined 
+ 
+ //console.table([age,employeName,accountLogin, userState])
+ console.log(typeof age)
+
+ /*
+data types 
+number = limit is 2 to 53
+but number are bigger then above limit then use 
+bigint
+ boolean -  yes or no
+ string - write in duble or single quote
+ null - assignt as null no value  (typeof null is object)
+ undefined (typeof undefined is undefined)
+
+ */
