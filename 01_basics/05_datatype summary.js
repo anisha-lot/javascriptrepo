d2a74109a572e0dda1abc4f4dd3040f11ee94f37
anisha-lot <anishalot@gmail.com>
2024-06-05 10:37:36 +0000

    delete unwanted

--- 01_basics/05_datatype summary.js
+++ 01_basics/05_datatype summary.js
@@ -38,25 +38,3 @@ let BigNumber = 15356673222332478n //this is bigInt data type
 // Object  =>  object
 
 
-
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-// stack and heap memory
-// stack use in primitive  and heap use in non-primitive
-
-let Name1 = "anisha"  //o/p is anisha this is orignal 
-let name2 = Name1
-name2= "lot"  
-console.log(name2)
-
-// non-primitive Heap
-
-let userOne= {
-    email:"anisha@gmail.com",
-    name:"anishalot",
-}
-let userTwo = userOne 
- userTwo.email ="anisha@gmail.com"
-
- console.log(userOne.email)
- console.log(userTwo.email)
